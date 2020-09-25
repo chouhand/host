452c280c3eb1f5831972b8d5f04048e6dec6391d
Deepika Chouhan <deepika.chouhan@talentica.com>
2020-09-26 01:26:09 +0530

    reset fn

--- src/Button.js
+++ src/Button.js
@@ -4,10 +4,13 @@ import './index.css'
 const Button = () => {
   const [counter, setcounter] = useState(0)
   const handlebtnClick = () => setcounter(counter + 1)
+  const handleResetClick = () => setcounter(0);
   return (
     <>
       <div>Clicked <span>{counter}</span></div>
       <button onClick={handlebtnClick}>Click</button>
+      <button onClick={handleResetClick}>Reset</button>
+
     </>
   )
 }
